61fb81c0f81c9be8a28a9719bf0201ff123c9a87
kingreebes <kingreebes@andys-mbp>
2018-10-13 20:31:48 -0500

    Customer Inserter

--- CH17/PC1_Customer_Inserter.java
+++ CH17/PC1_Customer_Inserter.java
@@ -7,38 +7,21 @@ public class PC1_Customer_Inserter {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		final String DB_URL = "jdbc:derby:CoffeeDB";
-		String name;
-		String address;
-		String city;
-		String state;
-		String zip;
+
 		
 		//Connect to DB
 		try {
 			Connection conn = DriverManager.getConnection(DB_URL);
 			
 			//Get Item info
-			System.out.println("Enter Customer Info");
-			Scanner keyboard = new Scanner(System.in);
-			System.out.println("Name: ");
-			name = keyboard.nextLine();
-
-			System.out.println("Address: ");
-			address = keyboard.nextLine();
-					
-			System.out.println("City: ");
-			city = keyboard.nextLine();
 			
-			System.out.println("State: ");
-			state = keyboard.nextLine();
-			
-			System.out.println("Zip: ");
-			zip = keyboard.nextLine();
 			Statement stmt = conn.createStatement();
-			stmt.executeUpdate(customerStatement(name, address, city, state, zip));
+			stmt.executeUpdate(inputRow());		
 			
+			//Setting up for display of table
 			ResultSet result = stmt.executeQuery("SELECT * FROM Customer");
 			ResultSetMetaData meta = result.getMetaData();
+			
 			while(result.next())
 			{
 				for(int i = 1; i <= meta.getColumnCount(); i++)
@@ -64,15 +47,94 @@ public class PC1_Customer_Inserter {
 				
 		
 	}
-	public static String customerStatement(String name, String address, String city, String state, String zip) {
+	/**
+	 *  Depreciated method that took user input from main and returned it in SQL from. Updated method follows.
+	 *  
+	 * @param name
+	 * @param address
+	 * @param city
+	 * @param state
+	 * @param zip
+	 * @return
+	 */
+	 
+	public static String customerStatement(String name, 
+										   String address, 
+										   String city, 
+										   String state, 
+										   String zip) 
+	{
+		//Generate random Customer Number
 		Random rand = new Random();
 		String customerNumber = Integer.toString(rand.nextInt(9999999));
 		
+		//Return SQL Statement
 		return "INSERT INTO Customer " +
 				"(CustomerNumber, Name, Address, City, State, Zip) " +
-				"VALUES ('" + customerNumber + "', '" + name + "', '" + address + "', '" + city
-				+ "', '" + state + "', '" + zip + "')";
+				"VALUES ('" + customerNumber + "', '" 
+				+ name + "', '" 
+				+ address + "', '" 
+				+ city
+				+ "', '" 
+				+ state + "', '" 
+				+ zip + "')";
 				
 	}
+	/**
+	 * Formats elements in array into an SQL statement
+	 * Returns to calling method
+	 * @param input
+	 * @return
+	 */
+	public static String customerStatement(String[] input)
+	{
+			//Generate random Customer Number
+			Random rand = new Random();
+			String customerNumber = Integer.toString(rand.nextInt(9999999));
+
+			//Return SQL Statement
+			return "INSERT INTO Customer " +
+			"(CustomerNumber, Name, Address, City, State, Zip) " +
+			"VALUES ('" + customerNumber + "', '" 
+			+ input[0] + "', '" 
+			+ input[1] + "', '" 
+			+ input[2]
+			+ "', '" 
+			+ input[3] + "', '" 
+			+ input[4] + "')";
+
+	}
+	/**
+	 * Gets information from user about the row entry
+	 * Sends to SQL statement method to format it properly
+	 * returns the SQL statement formatted properly
+	 * @return
+	 */
+	public static String inputRow() {
+		String[] input = new String[5];
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Enter Customer Info");
+
+		System.out.println("Name: ");
+		input[0] = keyboard.nextLine();
+
+		System.out.println("Address: ");
+		input[1] = keyboard.nextLine();
+				
+		System.out.println("City: ");
+		input[2] = keyboard.nextLine();
+		
+		System.out.println("State: ");
+		input[3] = keyboard.nextLine();
+		
+		System.out.println("Zip: ");
+		input[4] = keyboard.nextLine();
+		
+		String stmt = customerStatement(input);
+		return stmt;
+		
+	}
+	
 
 }
